0a64927de25b4e87ef33ed7ddc1fad672f6d03ac
Raju <rajutamang3456@gmail.com>
2025-04-30 15:55:54 +0545

    conditions

---
+++ js/conditions.js
@@ -0,0 +1,17 @@
+let isRain = false;
+let scorchingSun = false;
+let wilNotRain = true;
+let today = "wednesday";
+
+// if (isRain) {
+//   console.log("take umbrella");
+// } else if (scorchingSun) {
+//   console.log("take umbrella");
+// } else {
+//   console.log("dont take umbrella");
+// }
+
+if ((today = "sunday")) {
+  console.log("today is sunday");
+} else {
+}
