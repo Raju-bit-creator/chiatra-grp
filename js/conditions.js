0f02cc925494ba4d137ac8517ffd16c2c5dd8110
Raju <rajutamang3456@gmail.com>
2025-05-01 15:51:47 +0545

    operator

--- js/conditions.js
+++ js/conditions.js
@@ -1,7 +1,45 @@
-let isRain = false;
-let scorchingSun = false;
-let wilNotRain = true;
-let today = "wednesday";
+// Conditionals
+// IF ELSE
+// Ternary operator  ? :
+// Operands and operators
+// assignment
+// arithmetic
+// comparison < <= > >=  , == ,===
+// logical  &&  || !
+
+// let isRain = false;
+// let scorchingSun = false;
+// let wilNotRain = true;
+let today = "friday";
+
+// if (isRain) {
+//   console.log("take umbrella");
+// } else {
+//   console.log("no need to take umbrella");
+// }
+
+//teranary operator
+// let decision = today == "monday" ? "happy weekend" : "sad weekend";
+// console.log(decision);
+
+if (today == "sunday") {
+  console.log("today is sunday");
+} else if (today == "monday") {
+  console.log("today is monday");
+} else if (today == "tuesday") {
+  console.log("today is tuesday");
+} else if (today == "wednesday") {
+  console.log("today is wednesday");
+} else if (today == "thursday") {
+  console.log("today is thursday");
+} else {
+  console.log("today is friday");
+}
+let number = 4.5;
+let num = Math.floor(number);
+let num1 = Math.ceil(number);
+console.log(num);
+console.log(num1);
 
 // if (isRain) {
 //   console.log("take umbrella");
@@ -11,7 +49,34 @@ let today = "wednesday";
 //   console.log("dont take umbrella");
 // }
 
-if ((today = "sunday")) {
-  console.log("today is sunday");
+// assignment operator = > <
+//aritematic operator +_% / *
+//comparision  operator ==> >=== === ==
+//logical operator ! && ||
+let x = 2;
+const y = 3;
+
+// console.log(x);
+// // Expected output: 2
+
+// console.log((x = y + 1)); // 3 + 1
+// // Expected output: 4
+
+// console.log((x = x * y)); // 4 * 3
+// // Expected output: 12
+
+if (x > y) {
+  console.log("x is greater");
 } else {
+  console.log("y is greater");
+}
+
+let isRain = false;
+let scorchingSun = false;
+let wilNotRain = true;
+
+if (!isRain && !scorchingSun) {
+  console.log("take umbrella");
+} else {
+  console.log("no need to take umbrella");
 }
